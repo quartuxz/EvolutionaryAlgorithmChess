849b81eb3b38ee1f7eafca105cf8182415ee6108
matias chara <matiascharacronenbold@gmail.com>
2021-09-05 10:48:15 -0300

    started work in neural networks.

--- EvolutionaryAlgorithmChess/NeuralNetwork.cpp
+++ EvolutionaryAlgorithmChess/NeuralNetwork.cpp
@@ -1,1 +1,17 @@
 #include "NeuralNetwork.h"
+
+void Neuron::setValue(double val)
+{
+	m_value = val;
+}
+
+double Neuron::activate() const
+{
+	return 0.0;
+}
+
+void Neuron::reset()
+{
+	m_value = 0;
+	m_output = 0;
+}
--- EvolutionaryAlgorithmChess/NeuralNetwork.h
+++ EvolutionaryAlgorithmChess/NeuralNetwork.h
@@ -1,14 +1,38 @@
 #pragma once
 #include <vector>
 
+
+
+
+typedef std::vector<size_t> Topology;
+
+class Neuron;
+
+typedef std::pair<double, Neuron*> synapse;
+
+class Neuron {
+private:
+	std::vector<synapse> m_synapses;
+	double m_value;
+	double m_output;
+public:
+	Neuron();
+	void setValue(double val);
+	double activate()const;
+	void reset();
+};
+
+
 class NeuralNetwork
 {
 private:
-
+	
 public:
-	NeuralNetwork(std::vector<size_t> topology);
+	NeuralNetwork(Topology top);
 
-	double;
+
+	//sets the input layer to the passed values, pass them through the entire neural net, and get a vector of outputs.
+	std::vector<double> pass(const std::vector<double> &input)const;
 
 };
 
